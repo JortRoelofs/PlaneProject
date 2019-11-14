bfbedc86d9e28baafe0c83c3a408e281f21c2330
JayR1998 <jort.roelofs@gmail.com>
2019-11-14 14:20:30 +0100

    Add lift and drag function

--- Wing.py
+++ Wing.py
@@ -1,6 +1,8 @@
 
 class Wing:
 
+    v = 0
+
     X = 0
     Y = 0
     Z = 0
@@ -10,7 +12,8 @@ class Wing:
     CmGeom = 0
     CmQuarter = 0
 
-    def __init__(self, x, y, z, cp, cl, cd, cmgeom, cmquarter):
+    def __init__(self, v, x, y, z, cp, cl, cd, cmgeom, cmquarter):
+        self.v = v
         self.X = x
         self.Y = y
         self.Z = z
@@ -19,3 +22,9 @@ class Wing:
         self.Cd = cd
         self.CmGeom = cmgeom
         self.CmQuarter = cmquarter
+
+    def lift(self, y):
+        return 0.5 * 1.225 * self.v ** 2 * self.Cl(y) * self.X(y)
+
+    def drag(self, y):
+        return 0.5 * 1.225 * self.v ** 2 * self.Cl(y) * self.X(y)
--- WingLoader.py
+++ WingLoader.py
@@ -11,10 +11,10 @@ def loadWing(a, v):
     funcx = interpolate.interp1d(y, c, kind='linear', fill_value="extrapolate")
     funcy = interpolate.interp1d(y, y, kind='linear', fill_value="extrapolate")
     funcz = 0
-    funcCp = interpolate.interp1d(y, xcp, kind='nearest', fill_value="extrapolate")
-    funcCl = interpolate.interp1d(y, cl, kind='nearest', fill_value="extrapolate")
-    funcCd = interpolate.interp1d(y, cd, kind='nearest', fill_value="extrapolate")
-    funcCmGeom = interpolate.interp1d(y, cmgeom, kind='nearest', fill_value="extrapolate")
-    funcCmQuarter = interpolate.interp1d(y, cmquarter, kind='nearest', fill_value="extrapolate")
-    return Wing.Wing(funcx, funcy, funcz, funcCp, funcCl, funcCd, funcCmGeom, funcCmQuarter)
+    funcCp = interpolate.interp1d(y, xcp, kind='cubic', fill_value="extrapolate")
+    funcCl = interpolate.interp1d(y, cl, kind='cubic', fill_value="extrapolate")
+    funcCd = interpolate.interp1d(y, cd, kind='cubic', fill_value="extrapolate")
+    funcCmGeom = interpolate.interp1d(y, cmgeom, kind='cubic', fill_value="extrapolate")
+    funcCmQuarter = interpolate.interp1d(y, cmquarter, kind='cubic', fill_value="extrapolate")
+    return Wing.Wing(v, funcx, funcy, funcz, funcCp, funcCl, funcCd, funcCmGeom, funcCmQuarter)
 
